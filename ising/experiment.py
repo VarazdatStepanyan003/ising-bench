5c8b272a853c7c6933ad9dbdfa041fb3e0729f49
Varazdat Stepanyan <varostep@gmail.com>
2023-03-29 09:06:28 +0400

    patch 1.1
    Thanks LinusTechTips forum for the feedback <3

--- ising/experiment.py
+++ ising/experiment.py
@@ -4,15 +4,9 @@ from numba import njit
 
 
 @njit(nogil=True)
-def run():
-    n_of_steps = 250000
+def run(n, m):
+    n_of_steps = n
     max_time = 250
-    n_of_repetitions = 128
+    n_of_repetitions = m
     dt = 1
     times, observabless = many_simulate(n_of_steps=n_of_steps, max_time=max_time, n_of_repetitions=n_of_repetitions)
-
-    time, m = smooth(max_time, dt, times, observabless[:, :, 0])
-    time, e = smooth(max_time, dt, times, observabless[:, :, 1])
-
-    m = interpolate(m)
-    e = interpolate(e)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import time as current_time
 from ising import experiment
 
+experiment.run(1, 1)
 start_time = current_time()
-res = experiment.run()
+experiment.run(250000, 192)
 print("Score: ", 100000/float(current_time() - start_time))
